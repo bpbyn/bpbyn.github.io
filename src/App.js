eb6b5ec163f2c713eb318d0869233a4bf4bd6bb5
Brian <punongbayan.bri@gmail.com>
2021-02-08 20:48:02 +0800

    test commit for different user

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <div className="personal-info">
+          Test
           <Greetings/>
           <Navigation/>
           <Contact/>
